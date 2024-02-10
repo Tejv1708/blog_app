95c70e60eb22f52ab9f1439e26cc26541740af64
Tejv1708 <narukatejveer@gmail.com>
2024-02-10 09:59:08 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import postRouter from "./routes/posts.js";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser);
+app.use("/api/posts", postRouter);
+app.use("/api/auth", authRoutes);
+app.use("api/users", userRoutes);
+
+app.listen(8000, () => {
+  console.log("Connected! to localhost 8000");
+});
