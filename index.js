69ff72f00dbc6232b5b96cac6353b4ccd1392697
Tejv1708 <narukatejveer@gmail.com>
2024-02-11 13:03:07 +0530

    second commit

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ import postRouter from "./routes/posts.js";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-app.use(cookieParser);
+app.use(cookieParser());
+
 app.use("/api/posts", postRouter);
 app.use("/api/auth", authRoutes);
 app.use("api/users", userRoutes);
